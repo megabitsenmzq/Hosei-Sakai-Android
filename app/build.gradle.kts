68b804e0897d233a35570c0c6426d83cae747b49
Jinyu Meng <megabitsenmzq@gmail.com>
2023-12-28 02:23:16 +0900

    Bump version up.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.jinyumeng.hosei_sakai"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
